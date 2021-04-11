8d970268e149ad0bcc6077f9da821f5d414fe541
Kiran Kulkarni <kiran.bkulkarni@ucdconnect.ie>
2021-04-11 21:48:23 +0100

    updated the react child elements

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -4,18 +4,27 @@ import Counter from './counter'
 class Counters extends Component {
     state = {
         counters: [
-            { id: 1, value: 0 },
+            { id: 1, value: 4},
             { id: 2, value: 0},
             { id: 3, value: 0},
             { id: 4, value: 0}
         ]
 
-     }
+    }
+    
+    handleDelete = (counterID) => {
+        console.log('Handle Delete Event Called');
+        console.log(counterID)
+        const counters = this.state.counters.filter(c => c.id !== counterID);
+        this.setState({ counters });
+        console.log('Handle Delete Event Successful');
+    };
+
     render() { 
         return (
             <div>
                 {this.state.counters.map(counter =>
-                    <Counter key={counter.id} value ={counter.value} selected ={true}></Counter>)}
+                    <Counter key={counter.id} onDelete={this.handleDelete} counter={counter}></Counter>)}
             </div>
          );
     }
