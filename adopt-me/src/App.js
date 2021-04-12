c3e04ae86219768d4e386ae4055b2a674ca785a3
Kiran Kulkarni <kiran.bkulkarni@ucdconnect.ie>
2021-04-13 00:29:50 +0100

    Fifth Commit

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,30 +1,13 @@
 // your code is going to go here, for now
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParamas from "./SearchParamas";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Luna",
-  //     animal: "Dog",
-  //     breed: "Havaneses",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Pepper",
-  //     animal: "Bird",
-  //     breed: "Cockatiel",
-  //   }),
-  //   React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
-  // ]);
-
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel"></Pet>
-      <Pet name="Doink" animal="Cat" breed="Mixed"></Pet>
+      <SearchParamas></SearchParamas>
     </div>
   );
 };
