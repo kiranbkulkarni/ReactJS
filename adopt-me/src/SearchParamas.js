c890285c5f5f186493e6f7ab0b2c9a486c60bbb8
Kiran Kulkarni <kiran.bkulkarni@ucdconnect.ie>
2021-04-13 01:19:51 +0100

    Sixth Commit

--- adopt-me/src/SearchParamas.js
+++ adopt-me/src/SearchParamas.js
@@ -1,7 +1,12 @@
 import React, { Component, useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("breed", "", breeds);
 
   return (
     <div className="search-params">
@@ -16,6 +21,8 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
